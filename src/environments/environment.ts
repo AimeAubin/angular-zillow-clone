dac2e4135fadce7f918ab3b76885549df97f2286
Irakoze Aimé Aubin <aubin.irakoze@amalitech.com>
2024-08-21 16:42:13 +0200

    fix: remove environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  rapidApiUrl: 'https://zillow-com4.p.rapidapi.com/properties/search?location=new%20york',
-  rapidApiKey: 'f44af96c05mshdc7698481b6d83dp1a02fajsna276f6265c99',
-  rapidApidhost: 'zillow-com4.p.rapidapi.com',
-  authBaseUrl: 'https://go-auth-v1.onrender.com/user',
-  xapn: 'e85dde16b0c15d9847ee97db29b42eab',
+  rapidApiUrl: '',
+  rapidApiKey: '',
+  rapidApidhost: '',
+  authBaseUrl: '',
+  xapn: '',
   };
